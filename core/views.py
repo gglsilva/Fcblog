cd4e3b57aa8eb2b6027abe5c5f22822b3c6fbc12
gglsilva <gabrielchamex@gmail.com>
2024-10-22 19:48:50 -0300

    finaliza projeto de blog

--- core/views.py
+++ core/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
+from .models import Post, Tag
 
 # Create your views here.
+def index(request):
+    posts = Post.objects.prefetch_related('tags').all()
+    context = {
+        'posts': posts,
+    }
+    return render(request, 'core/index.html', context)
