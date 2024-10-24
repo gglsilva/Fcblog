bad5f175f9bf85b885f5788c7615caac09af3cd3
gglsilva <gabrielchamex@gmail.com>
2024-10-24 11:31:50 -0300

    fix: redireciona para rota/blog

--- fcblog/urls.py
+++ fcblog/urls.py
@@ -15,9 +15,13 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
+from django.shortcuts import redirect
 from django.urls import path, include
+from core import views
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('',include('core.urls')),
+    path('', lambda request: redirect('index', permanent=False)),  
+    path('', include('core.urls')), 
 ]
